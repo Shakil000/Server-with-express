e1a2ca42086ed784d55860171058829eca1eda2d
Shakil000 <racer.shakil111@gmail.com>
2025-06-01 18:08:15 +0600

    update

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,9 @@
 //const express = require('express')
-import express from 'express';
-const app = express()
+import express, { Application, Request, Response } from 'express';
+const app : Application = express()
 const port = 3000
 
-app.get('/', (req, res) => {
+app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!')
 })
 
