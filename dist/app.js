b2765040995128b6a3a5e65b35190651123cc509
Shakil000 <racer.shakil111@gmail.com>
2025-06-17 14:09:33 +0600

    update

--- dist/app.js
+++ dist/app.js
@@ -5,9 +5,23 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 //const express = require('express')
 const express_1 = __importDefault(require("express"));
+const fs_1 = __importDefault(require("fs"));
+const path_1 = __importDefault(require("path"));
 const app = (0, express_1.default)();
+app.use(express_1.default.json());
+const filePath = path_1.default.join(__dirname, "../db/todo.json");
 app.get('/', (req, res) => {
     // console.log({req, res})
-    res.send('I am learning express js and I also want to learn node and other');
+    res.send('I am learning express js and I also want to learn node and others because I love to do code with node and express');
+});
+app.get('/todos', (req, res) => {
+    const data = fs_1.default.readFileSync(filePath, { encoding: "utf-8" });
+    console.log(data);
+    res.json(data);
+});
+app.post('/todos/create-todo', (req, res) => {
+    const { title, body } = req.body;
+    console.log(title, body);
+    res.send('third one');
 });
 exports.default = app;
--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,26 @@
 //const express = require('express')
 import express, { Application, Request, Response } from 'express';
-const app : Application = express()
+import fs from "fs";
+import path from 'path';
+const app : Application = express();
+
+app.use(express.json());
+
+const filePath = path.join(__dirname,"../db/todo.json");
 
 app.get('/', (req: Request, res: Response) => {
   // console.log({req, res})
-  res.send('I am learning express js and I also want to learn node and other')
+  res.send('I am learning express js and I also want to learn node and others because I love to do code with node and express')
+})
+app.get('/todos', (req: Request, res: Response) => {
+  const data = fs.readFileSync(filePath,{encoding: "utf-8"})
+  console.log(data);
+  res.json(data);
+})
+app.post('/todos/create-todo', (req: Request, res: Response) => {
+  const {title, body} = req.body;
+  console.log(title, body);
+  res.send('third one');
 })
 
 
